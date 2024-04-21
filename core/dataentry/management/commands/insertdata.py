f33cfb5697708ab4c3b393b34c09b7aed790fc47
Alireza Anbari <alireza.anbary@gmail.com>
2024-04-21 11:14:03 +0330

    Existing validation implement.

--- core/dataentry/management/commands/insertdata.py
+++ core/dataentry/management/commands/insertdata.py
@@ -14,18 +14,27 @@ class Command(BaseCommand):
         # Add 1 data
         # Student.objects.create(roll_no=1001,name='Alireza',age=27)
         
-        # Add more data
+        # Add bunch of data
         dataset = [
-            {'roll_no': 1002, 'name': 'saman', 'age':21},
-            {'roll_no': 1003, 'name': 'sadaf', 'age':25},
-            {'roll_no': 1004, 'name': 'sina', 'age':30},
+            {'roll_no': 1008, 'name': 'marjan', 'age':21},
+            {'roll_no': 1009, 'name': 'sama', 'age':25},
+            {'roll_no': 1010, 'name': 'mansour', 'age':30},
         ]
         
+        stdent_count = 0
         for data in dataset:
-            Student.objects.create(
-                roll_no=data['roll_no'],
-                name=data['name'],
-                age=data['age'],
-            )
-        
-        self.stdout.write(self.style.SUCCESS('Data inserted successfully.'))
+            roll_no = data['roll_no']
+            existing_roll_no_record = Student.objects.filter(roll_no=roll_no).exists()
+            
+            if not existing_roll_no_record:
+                Student.objects.create(
+                    roll_no=data['roll_no'],
+                    name=data['name'],
+                    age=data['age'],
+                )
+                
+                stdent_count += 1
+            else:
+                self.stdout.write(self.style.WARNING(f'Student with roll no {roll_no} already exist!'))
+                
+        self.stdout.write(self.style.SUCCESS(f'Data inserted successfully.\n{stdent_count} added.'))
