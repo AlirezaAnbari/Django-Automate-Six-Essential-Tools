a1126f82cd077710da092ae88765194d21d8ac9a
Alireza Anbari <alireza.anbary@gmail.com>
2024-04-28 08:48:08 +0330

    Storing the uploaded files.

--- core/core/settings.py
+++ core/core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     
     # app
     "dataentry",
+    "uploads",
     
 ]
 
@@ -120,6 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+
+# Media files
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- core/core/urls.py
+++ core/core/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -26,3 +28,13 @@ urlpatterns = [
     path("dataentry/", include("dataentry.urls")),
     
 ]
+
+
+# Serving static and media for development
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT
+    )
