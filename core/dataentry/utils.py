56fa05fff92ec73d1e538deda946c75217109b27
Alireza Anbari <alireza.anbary@gmail.com>
2024-04-28 11:06:20 +0330

    Trigger the importdata command to view function.

--- core/dataentry/utils.py
+++ core/dataentry/utils.py
@@ -3,13 +3,17 @@ from django.apps import apps as django_apps
 
 
 def get_all_custom_models():
-    default_models = [
-        'ContentType', 'Session', 'LogEntry', 'Group', 'Permission',
-    ]
+    # default_models = [
+    #     'ContentType', 'Session', 'LogEntry', 'Group', 'Permission',
+    # ]
+    default_models = [model.__name__ for model in apps.get_models() if model.__module__.startswith('django.contrib')]
+    exclude_models = ['Upload']
+    exclude_models += default_models
     
     custom_models = []
     for model in apps.get_models():
-        if not model.__module__.startswith('django.contrib'): # exclude default django models
+        # if not model.__module__.startswith('django.contrib') and  not exclude_models: # exclude default django models and upload
+        #     custom_models.append(model.__name__)
+        if model.__name__ not in exclude_models:
             custom_models.append(model.__name__)
-            
     return custom_models
--- core/dataentry/views.py
+++ core/dataentry/views.py
@@ -1,11 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.core.management import call_command
 
 from .utils import get_all_custom_models
+from uploads.models import Upload
 
 
 def import_data(request):
     if request.method == 'POST':
-        return
+        file_path = request.FILES['file_path']
+        model_name = request.POST['model_name']
+        
+        # store this file inside the upload model
+        upload = Upload.objects.create(file=file_path, model_name=model_name)
+        
+        # construct the full path
+        relative_path = str(upload.file.url)
+        base_url = str(settings.BASE_DIR)
+        
+        file_full_path = base_url + relative_path
+        
+        # trigger the importdata command to view
+        try:
+            call_command('importdata', file_full_path, model_name)
+        except Exception as e:
+            raise e
+        
+        return redirect('import_data')
     else:
         # Get all the custom models in each apps(utils.py)
         custom_models = get_all_custom_models()
